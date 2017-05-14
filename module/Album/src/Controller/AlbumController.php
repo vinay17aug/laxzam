b9a6babca3842bb4763fc2716bae9dc53e0cfad0
Vinay Kumar Sharma <sharma.vinay@ourdreamrun.com>
2017-05-14 21:04:31 +0530

    63 Files Total
    Signed-off-by: Vinay Kumar Sharma <sharma.vinay@ourdreamrun.com>

---
+++ module/Album/src/Controller/AlbumController.php
@@ -0,0 +1,38 @@
+<?php
+namespace Album\Controller;
+
+// Add the following import:
+use Album\Model\AlbumTable;
+use Zend\Mvc\Controller\AbstractActionController;
+use Zend\View\Model\ViewModel;
+
+class AlbumController extends AbstractActionController
+{
+    // Add this property:
+    private $table;
+
+	// Add this constructor:
+    public function __construct(AlbumTable $table)
+    {
+        $this->table = $table;
+    }
+	
+	public function indexAction()
+    {
+		return new ViewModel([
+            'albums' => $this->table->fetchAll(),
+        ]);
+    }
+
+    public function addAction()
+    {
+    }
+
+    public function editAction()
+    {
+    }
+
+    public function deleteAction()
+    {
+    }
+}
